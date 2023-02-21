41aef995d6ae0967895fca35493743e94ef8d2c2
Alef oliveira <alefolivfeira@gmail.com>
2023-02-20 23:08:16 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,95 @@
+import { useCallback } from 'react';
+import ReactFlow, { addEdge, Background, Connection, ConnectionMode, Controls, Node, useEdgesState, useNodesState } from 'reactflow';
+import 'reactflow/dist/style.css';
+
+import * as Toolbar from '@radix-ui/react-toolbar';
+import DefaultEdge from './components/edges/DefaultEdge';
+import { Square } from './components/nodes/Square';
+
+const NODE_TYPES = {
+  square: Square,
+}
+
+const EDGE_TYPES = {
+  default: DefaultEdge,
+}
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 200,
+      y: 400
+    },
+    data: {}
+  },
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 800,
+      y: 400
+    },
+    data: {}
+  }
+]satisfies Node[]
+
+function App() {
+
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES);
+
+  const onConnect = useCallback((connection: Connection) => {
+    return setEdges(edges => addEdge(connection, edges))
+  }, [])
+
+
+
+  const addSquareNode = () => {
+    setNodes(nodes => [
+      ...nodes, {
+        id: crypto.randomUUID(),
+        type: 'square',
+        position: {
+          x: 854,
+          y: 350,
+        },
+        data: {}
+      },
+    ])
+  }
+
+  return (
+    <div className="w-screen h-screen">
+      <ReactFlow
+        edgeTypes={EDGE_TYPES}
+        nodeTypes={NODE_TYPES}
+        nodes={nodes}
+        edges={edges}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onNodesChange={onNodesChange}
+        connectionMode={ConnectionMode.Loose}
+        defaultEdgeOptions={{
+          type: 'default'
+        }}
+      >
+        <Background
+          gap={12}
+          size={2}
+          color="#ddd"
+        />
+        <Controls />
+      </ReactFlow>
+      {/* 
+      <Toolbar.Root className='fixed bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-2xl shadow-lg border border-zinc-300 px-8 h-20 w-96 overflow-hidden ' >
+        <Toolbar.Button onClick={addSquareNode} className='w-32 h-32 bg-slate-500 mt-6 rounded transition-transform hover:-translate-y-2' />
+      </Toolbar.Root> */}
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/nodes/Square.tsx
@@ -0,0 +1,37 @@
+import { NodeProps, Handle, Position } from 'reactflow'
+
+export const Square = (props: NodeProps) => {
+    return (
+        <div className="bg-violet-500 rounded w-[200px] h-[200px]">
+            <Handle
+                position={Position.Right}
+                id="right"
+                type={'source'}
+                className="-right-5 w-3 h-3 bg-transparent bg-blue-400/70"
+            />
+
+            <Handle
+                position={Position.Left}
+                id="left"
+                type={'source'}
+                className="-left-5 w-3 h-3 bg-transparent bg-blue-400/70"
+
+            />
+
+            <Handle
+                position={Position.Top}
+                id="top"
+                type={'source'}
+                className="-top-5 w-3 h-3 bg-transparent bg-blue-400/80"
+            />
+
+            <Handle
+                position={Position.Bottom}
+                id="bottom"
+                type={'source'}
+                className="-bottom-5 w-3 h-3 bg-transparent bg-blue-400/80"
+
+            />
+        </div>
+    )
+}
